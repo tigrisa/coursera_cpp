406d9a98c4a65545284bb17476f2762efb0218e8
tigrisa <closefulya@gmail.com>
2020-09-20 00:07:32 +0300

    RedBelt LinkedList END WEEK 3

---
+++ RedBelt/VS Code/WEEK3/linked_list.cpp
@@ -0,0 +1,146 @@
+#include "../includes/test_runner.h"
+
+#include <vector>
+using namespace std;
+
+template <typename T>
+class LinkedList {
+public:
+  struct Node {
+    T value;
+    Node* next = nullptr;
+  };
+
+  ~LinkedList(){
+    Node * current = head;
+    while ( current ){
+      Node * buf = current->next;
+      delete current;
+      current = buf;
+    }
+  }
+
+  void PushFront(const T& value){
+    Node * buf = head;
+    head = new Node{value,buf};
+  }
+
+  void InsertAfter(Node* node, const T& value){
+    if ( node != nullptr )
+    {
+      Node * buf = node->next;
+      Node * newNode = new Node{value,buf};
+      node->next = newNode;
+    }
+    else PushFront(value);
+  }
+
+
+  void RemoveAfter(Node* node) {
+    if ( node != nullptr )
+    {
+      Node * buf = node->next;
+      if ( buf != nullptr ){
+        Node * delbuf = buf->next;
+        delete buf;
+        node->next = delbuf;
+      }
+    }
+    else PopFront();
+  }
+
+  void PopFront(){
+    Node * buf = head->next;
+    delete head;
+    head = buf;
+  }
+
+  Node* GetHead() { return head; }
+  const Node* GetHead() const { return head; }
+
+private:
+  Node* head = nullptr;
+};
+
+template <typename T>
+vector<T> ToVector(const LinkedList<T>& list) {
+  vector<T> result;
+  for (auto node = list.GetHead(); node; node = node->next) {
+    result.push_back(node->value);
+  }
+  return result;
+}
+
+void TestPushFront() {
+  LinkedList<int> list;
+
+  list.PushFront(1);
+  ASSERT_EQUAL(list.GetHead()->value, 1);
+  list.PushFront(2);
+  ASSERT_EQUAL(list.GetHead()->value, 2);
+  list.PushFront(3);
+  ASSERT_EQUAL(list.GetHead()->value, 3);
+
+  const vector<int> expected = {3, 2, 1};
+  ASSERT_EQUAL(ToVector(list), expected);
+}
+
+void TestInsertAfter() {
+  LinkedList<string> list;
+
+  list.PushFront("a");
+  auto head = list.GetHead();
+  ASSERT(head);
+  ASSERT_EQUAL(head->value, "a");
+
+  list.InsertAfter(head, "b");
+  const vector<string> expected1 = {"a", "b"};
+  ASSERT_EQUAL(ToVector(list), expected1);
+
+  list.InsertAfter(head, "c");
+  const vector<string> expected2 = {"a", "c", "b"};
+  ASSERT_EQUAL(ToVector(list), expected2);
+}
+
+void TestRemoveAfter() {
+  LinkedList<int> list;
+  for (int i = 1; i <= 5; ++i) {
+    list.PushFront(i);
+  }
+
+  const vector<int> expected = {5, 4, 3, 2, 1};
+  ASSERT_EQUAL(ToVector(list), expected);
+
+  auto next_to_head = list.GetHead()->next;
+  list.RemoveAfter(next_to_head); // удаляем 3
+  list.RemoveAfter(next_to_head); // удаляем 2
+
+  const vector<int> expected1 = {5, 4, 1};
+  ASSERT_EQUAL(ToVector(list), expected1);
+
+  while (list.GetHead()->next) {
+    list.RemoveAfter(list.GetHead());
+  }
+  ASSERT_EQUAL(list.GetHead()->value, 5);
+}
+
+void TestPopFront() {
+  LinkedList<int> list;
+
+  for (int i = 1; i <= 5; ++i) {
+    list.PushFront(i);
+  }
+  for (int i = 1; i <= 5; ++i) {
+    list.PopFront();
+  }
+  ASSERT(list.GetHead() == nullptr);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestPushFront);
+  RUN_TEST(tr, TestInsertAfter);
+  RUN_TEST(tr, TestRemoveAfter);
+  RUN_TEST(tr, TestPopFront);
+  return 0;
+}
